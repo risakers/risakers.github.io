5a78400347080218fdbaefc13ebed963ccb8ef6e
Marisa <ris.akers@gmail.com>
2015-12-14 17:49:50 -0500

    fix cards and add photoset grid

--- art.js
+++ art.js
@@ -1,3 +1,15 @@
-$('img').click(function () {
-        $(this).width(500);
+$('.photoset-grid-lightbox').photosetGrid({
+  highresLinks: true,
+  rel: 'withhearts-gallery',
+  gutter: '2px',
+
+  onComplete: function(){
+    $('.photoset-grid-lightbox').attr('style', '');
+    $('.photoset-grid-lightbox a').colorbox({
+      photo: true,
+      scalePhotos: true,
+      maxHeight:'90%',
+      maxWidth:'90%'
     });
+  }
+});
